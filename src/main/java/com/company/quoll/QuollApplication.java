002bef000afb12615bef131d29a3d1d513c9109b
Martin Polreich <polreichmartin@gmail.com>
2017-11-09 20:06:42 +0100

    Re-initialize

---
+++ src/main/java/com/company/quoll/QuollApplication.java
@@ -0,0 +1,12 @@
+package com.company.quoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuollApplication.class, args);
+	}
+}
---
+++ src/test/java/com/company/quoll/QuollApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.quoll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuollApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
