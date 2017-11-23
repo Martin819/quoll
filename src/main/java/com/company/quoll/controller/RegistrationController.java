63a5a89d8ddb1d80cb7c533de98a65727e0bd9bc
Ondra <ondrej@jacon.cz>
2017-11-23 10:54:55 +0100

    registration form (new object)

--- src/main/java/com/company/quoll/controller/RegistrationController.java
+++ src/main/java/com/company/quoll/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.company.quoll.controller;
 
 import com.company.quoll.model.Address;
+import com.company.quoll.model.RegistrationForm;
 import com.company.quoll.model.User;
 import com.company.quoll.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,30 +26,46 @@ public class RegistrationController {
     public String getForm(Model model) {
         final List<Address> countries = addressService.findAddresses(null, 0);
         model.addAttribute("countries", countries);
-        User user = new User();
-        model.addAttribute("user", user);
+//        User user = new User();
+//        model.addAttribute("user", user);
+        RegistrationForm registrationForm = new RegistrationForm();
+        model.addAttribute("registrationForm", registrationForm);
         // TODO address
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String submitForm(@Valid User user, BindingResult bindingResult) {
+    public String submitForm(@Valid RegistrationForm registrationForm, BindingResult bindingResult) {
+        System.out.println("SUBMIT");
+        System.out.println(registrationForm.getUsername());
+        System.out.println(registrationForm.getDateOfBirth());
+        System.out.println(registrationForm.getEmail());
+        System.out.println(registrationForm.getPassword());
         if (bindingResult.hasErrors()) {
-            System.out.println("hasErrors");
-            System.out.println(user.getUsername());
-            System.out.println(user.getDateOfBirth());
-            System.out.println(user.getEmail());
-            System.out.println(user.getPassword());
             System.out.println(bindingResult.getAllErrors().toString());
             return "registration";
         }
-        System.out.println("does not have errors");
-        System.out.println(user.getUsername());
-        System.out.println(user.getDateOfBirth());
-        System.out.println(user.getEmail());
-        System.out.println(user.getPassword());
         return "redirect:/";
     }
+
+//    @PostMapping("/registration")
+//    public String submitForm(@Valid User user, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            System.out.println("hasErrors");
+//            System.out.println(user.getUsername());
+//            System.out.println(user.getDateOfBirth());
+//            System.out.println(user.getEmail());
+//            System.out.println(user.getPassword());
+//            System.out.println(bindingResult.getAllErrors().toString());
+//            return "registration";
+//        }
+//        System.out.println("does not have errors");
+//        System.out.println(user.getUsername());
+//        System.out.println(user.getDateOfBirth());
+//        System.out.println(user.getEmail());
+//        System.out.println(user.getPassword());
+//        return "redirect:/";
+//    }
 
     @GetMapping("/registration/nuts1")
     public @ResponseBody List<Address> getNuts1(@RequestParam String nuts0) {
