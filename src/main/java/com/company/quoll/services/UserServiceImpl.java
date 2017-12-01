64ca81e22bb11e4afb5a7776badecead852e169c
Martin Polreich <polreichmartin@gmail.com>
2017-12-01 20:47:06 +0100

    Update User, fix NotBlanks and update Data.sql

--- src/main/java/com/company/quoll/services/UserServiceImpl.java
+++ src/main/java/com/company/quoll/services/UserServiceImpl.java
@@ -67,7 +67,7 @@ public class UserServiceImpl implements UserService {
     public void saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(1);
-        Role userRole = roleRepository.findByRole("USER");
+        Role userRole = roleRepository.findByRole("ROLE_USER");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         user.setZodiacSign(ZodiacSigns.getZodiacSign(user.getDateOfBirth()));
         userRepository.save(user);
