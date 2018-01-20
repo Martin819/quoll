922d513e17dfe0c7e80634906a1662d1c97fa37d
Martin Polreich <polreichmartin@gmail.com>
2018-01-20 15:02:54 +0100

    Add Apache Common Collections library and fix conditions

--- src/main/java/com/company/quoll/utils/MessagesUtils.java
+++ src/main/java/com/company/quoll/utils/MessagesUtils.java
@@ -7,15 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.commons.collections4.CollectionUtils;
 
 public class MessagesUtils {
     public static List<User> getUniqueContacts(List<Message> messagesFrom, List<Message> messagesTo) {
         List<User> uniqueContacts = new ArrayList<>();
-        List<User> contactsFrom = null;
-        List<User> contactsTo = null;
+        List<User> contactsFrom = new ArrayList<>();
+        List<User> contactsTo = new ArrayList<>();
         List<User> allContacts = new ArrayList<>();
-        if (!messagesFrom.isEmpty() && !messagesTo.isEmpty()) {
+        if (CollectionUtils.isNotEmpty(messagesFrom) && CollectionUtils.isNotEmpty(messagesTo)) {
             for (Message message:messagesFrom) {
+                System.out.println(message.getContent());
                 contactsFrom.add(message.getSender());
             }
             for (Message message:messagesTo) {
@@ -23,9 +25,9 @@ public class MessagesUtils {
             }
             Stream.of(contactsFrom, contactsTo).forEach(allContacts::addAll);
             uniqueContacts = allContacts.stream().distinct().collect(Collectors.toList());
-        } else if (messagesFrom.isEmpty() && messagesTo.isEmpty()) {
+        } else if (CollectionUtils.isEmpty(messagesFrom) && CollectionUtils.isEmpty(messagesTo)) {
             return uniqueContacts;
-        } else if (messagesFrom.isEmpty()) {
+        } else if (CollectionUtils.isEmpty(messagesFrom)) {
             for (Message message:messagesTo) {
                 contactsTo.add(message.getRecipient());
             }
@@ -41,11 +43,11 @@ public class MessagesUtils {
 
     public static List<Message> getAllMessages(List<Message> messagesFrom, List<Message> messagesTo){
         List<Message> allMessages = new ArrayList<>();
-        if (!messagesFrom.isEmpty() && !messagesTo.isEmpty()) {
+        if (CollectionUtils.isNotEmpty(messagesFrom) && CollectionUtils.isNotEmpty(messagesTo)) {
             Stream.of(messagesFrom, messagesTo).forEach(allMessages::addAll);
-        } else if (messagesFrom.isEmpty() && messagesTo.isEmpty()) {
+        } else if (CollectionUtils.isEmpty(messagesFrom) && CollectionUtils.isEmpty(messagesTo)) {
             return allMessages;
-        } else if (messagesFrom.isEmpty()) {
+        } else if (CollectionUtils.isEmpty(messagesFrom)) {
             allMessages = messagesTo;
         } else {
             allMessages = messagesFrom;
